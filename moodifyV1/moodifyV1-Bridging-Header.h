43cb87e42dd00bb2608434b019b53df0b3fe7a39
shelbymarcus <drmeilu872>
2019-01-31 20:35:47 -0800

    added project

---
+++ moodifyV1/moodifyV1-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  moodifyV1-Bridging-Header.h
+//  moodifyV1
+//
+//  Created by Shelby Marcus on 1/30/19.
+//  Copyright © 2019 Shelby Marcus. All rights reserved.
+//
+
+#ifndef moodifyV1_Bridging_Header_h
+#define moodifyV1_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* moodifyV1_Bridging_Header_h */
