d6e8102fce78d9cba36a15c3b0b4197e941b3857
Isi Okojie <Isi@Isis-MacBook-Pro.local>
2019-02-01 14:26:25 -0800

    Add new project.

---
+++ README.md
@@ -0,0 +1,36 @@
+# moodify
+
+## Team Members
+
+| Ashi | Dylan | Isi | Shelby | Zoe |
+| --- | --- | --- | --- | --- |
+| ![alt text](team-photos/ashi.png) | ![alt text](team-photos/dylan.png) | ![alt text](team-photos/isi.png) | ![alt text](team-photos/shelby.png) | ![alt text](team-photos/zoe.png) |
+
+## Team Skills Matrix
+
+| Member | Skills | Personal Traits | Desired Growth | Weaknesses |
+|:---:| --- | --- | --- | --- |
+| Ashi | Python, WebDev, Testing | Extroverted, optimistic, hard-working | AI/ML, rapid prototyping | Consistency in work sessions, sleep |
+| Dylan | UX/UI, Design, Needfinding | Open-minded, cooperative, motivated | Front end, mobile/web development | Confidence, experience building full product |
+| Isi | AI/ML, Data Science, Python, Illustrator/Photoshop | Hard-working, cooperative, easy-going | Building a full system from scratch, web development | Front end, web/app development, clearly elaborating thoughts |
+| Shelby | AI/ML, Data Science, Python | dog person | Web development | dog person |
+| Zoe | Databases, Java, Object oriented design, some ML | Organized, collaborative, initiative-taking, hard-working | front end/ scripting, ios, more AI | Confidence, music knowledge (relevant if we go that direction) |
+
+
+## Team Communication
+
+1. Slack
+
+2. Email
+
+| Name | Email |
+|:---:|:---:|
+| Ashi | ashia@stanford.edu |
+| Dylan | dharding@stanford.edu |
+| Isi | iokojie@stanford.edu |
+| Shelby | shelbym@stanford.edu |
+| Zoe | zpacalin@stanford.edu |
+
+## SGM Form
+
+[Google Form](https://docs.google.com/forms/d/e/1FAIpQLSe8deyBZFaX0HOXE5-4cS2L4s1Uiksq0lLBqDyfmdt8Dr7hIQ/viewform?usp=sf_link)
--- moodifyV2/ViewController.swift
+++ moodifyV2/ViewController.swift
@@ -9,7 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    fileprivate let SpotifyClientID = "a9e85dbb91464cf08f68d59908cc496c"
+    fileprivate let SpotifyRedirectURI = URL(string: "moodify://spotify-login-callback")!
 
+    @IBAction func AuthenticateButton(_ sender: Any) {
+        getAuth()
+    }
+    
+    func getAuth() {
+        //let url = URL(string:"https://accounts.spotify.com/authorize")
+        
+        let url = URL(string: "https://accounts.spotify.com/login?continue=https%3A%2F%2Faccounts.spotify.com%2Fauthorize")
+        
+        var request = URLRequest(url: url!)
+        
+        request.addValue(SpotifyClientID, forHTTPHeaderField: "client_id")
+        //request.addValue("code", forHTTPHeaderField: "response_type")
+        //request.addValue("moodify://spotify-login-callback", forHTTPHeaderField: "redirect_uri")
+
+        //var responseURL = URL(string:"")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            print(response)
+        }
+        task.resume()
+        
+        
+        //return(responseURL!)
+        //print(responseURL)
+        /*
+        let task2 = URLSession.shared.dataTask(with: responseURL!) { data, response, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            print(response?.url)
+            //print("about to do Json")
+            //let json = try! JSONSerialization.jsonObject(with: data, options: [])
+            //print(json)
+        }
+        task2.resume()
+         */
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
