dda258604d07343b467199a7fd6044ae7d22445e
Isi Okojie <Isi@isis-macbook-pro.local>
2019-01-31 21:58:54 -0800

    Initial Commit

---
+++ moodifyV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moodifyV2
+//
+//  Created by Isi Okojie on 1/31/19.
+//  Copyright © 2019 Isi Okojie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
