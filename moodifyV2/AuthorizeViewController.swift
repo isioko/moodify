5a082eb3752f5de17cb236b12af4703d4e62d325
Isi Okojie <Isi@isis-macbook-pro.local>
2019-02-12 13:11:26 -0800

    update authorize page

--- moodifyV2/AuthorizeViewController.swift
+++ moodifyV2/AuthorizeViewController.swift
@@ -10,6 +10,13 @@ import Foundation
 import UIKit
 
 class AuthorizeViewController: UIViewController{
+    // Colors for gradient
+    let pinkColor = UIColor(red: 250/225, green: 104/225, blue: 104/225, alpha: 1).cgColor
+    let purpleColor = UIColor(red: 179/225, green: 102/225, blue: 225/225, alpha: 1).cgColor
+    let blueColor = UIColor(red: 85/225, green: 127/225, blue: 242/225, alpha: 1).cgColor
+    
+    let gradient = CAGradientLayer()
+    @IBOutlet weak var gradientView: UIView!
     
     @IBOutlet weak var authorizeApp: UIButton!
     
@@ -22,5 +29,12 @@ class AuthorizeViewController: UIViewController{
         // Authorize our app for the Spotify account if there is no token
         // This opens a browser window from which the user can authenticate into his account
         spotifyManager.authorize()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //add gradient to background
+        gradient.frame = gradientView.bounds
+        gradient.colors = [pinkColor, purpleColor, blueColor]
+        gradientView.layer.insertSublayer(gradient, at: 0)
     }
 }
