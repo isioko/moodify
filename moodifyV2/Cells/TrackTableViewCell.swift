bb06af953a1cc44af9d282f3ab35fe7d19b389b2
Isi Okojie <Isi@isis-macbook-pro.local>
2019-02-22 12:40:45 -0800

    implement icon accessory view in recently played music pop up

--- moodifyV2/Cells/TrackTableViewCell.swift
+++ moodifyV2/Cells/TrackTableViewCell.swift
@@ -14,7 +14,8 @@ class TrackTableViewCell: UITableViewCell {
     @IBOutlet weak var trackNameLabel: UILabel!
     @IBOutlet weak var trackArtworkImage: UIImageView!
     @IBOutlet weak var artistNameLabel: UILabel!
-
+    @IBOutlet weak var checkmarkView: UIImageView!
+    
     func displayTrack(track: Track){
         trackNameLabel.text = track.trackName
         artistNameLabel.text = track.artistName
