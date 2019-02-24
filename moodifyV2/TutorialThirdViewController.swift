650fc05bf4970b8bde7c18ad36f044a2093786a3
Dylan Harding <dylanharding@dylans-macbook-pro-5.local>
2019-02-23 21:11:11 -0800

    connected gradient view to tutorial pages

---
+++ moodifyV2/TutorialThirdViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TutorialThirdViewController.swift
+//  moodifyV2
+//
+//  Created by Dylan Harding on 2/23/19.
+//  Copyright © 2019 Isi Okojie. All rights reserved.
+//
+
+import UIKit
+
+class TutorialThirdViewController: UIViewController {
+
+    @IBOutlet weak var gradientView: UIView!
+    let gradient = CAGradientLayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        gradient.frame = gradientView.bounds
+        gradient.colors = Constants.themeColors()
+        gradientView.layer.insertSublayer(gradient, at: 0)
+        // Do any additional setup after loading the view.
+    }
+}
