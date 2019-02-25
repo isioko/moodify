0e284ed12424d559b97522a11ab42f427158a020
Zoe Pacalin <zoepacalin@yahoo.com>
2019-02-25 00:34:44 -0800

    Dear Dylan -- BLESS YOUR SOUL for asking me to add swipe function to the tutorial. my struggles have become worth something yayyy

--- moodifyV2/TutorialFirstViewController.swift
+++ moodifyV2/TutorialFirstViewController.swift
@@ -13,6 +13,17 @@ class TutorialFirstViewController: UIViewController {
     @IBOutlet weak var gradientView: UIView!
     let gradient = CAGradientLayer()
     
+    @IBAction func handleSwipe(recognizer:UISwipeGestureRecognizer) {
+        print("yay swipe came thru")
+        
+//        let translation = recognizer.translation(in: self.view)
+//        if let view = recognizer.view {
+//            view.center = CGPoint(x:view.center.x + translation.x,
+//                                  y:view.center.y + translation.y)
+//        }
+//        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         gradient.frame = gradientView.bounds
